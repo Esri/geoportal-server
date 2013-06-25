fcac22c50840cf31207e54bffc775f086aafc8a7
Pratik Sanyal <pratiksynl@yahoo.co.in>
2013-06-24 17:17:36 -0700

    Deleting to reload full project.

--- components/gc/src/gc/base/task/TaskResult.java
+++
@@ -1,5 +0,0 @@
-package gc.base.task;
-
-public class TaskResult {
-
-}
--- components/gc/src/gc/base/util/ValueUtil.java
+++
@@ -1,9 +0,0 @@
-package gc.base.util;
-
-public class ValueUtil {
-
-	public static String trim(String s) {
-		if (s != null) s = s.trim();
-		return s;
-	}
-}
