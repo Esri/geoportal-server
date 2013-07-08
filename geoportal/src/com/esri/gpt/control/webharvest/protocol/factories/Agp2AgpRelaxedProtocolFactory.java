c401fec2724474c719f148d2089a0c59f2960960
Piotr Andzel <pandzel@esri.com>
2013-07-08 07:24:12 -0700

    Header and JavaDoc added to Agp2AgpRelaxedProtocolFactory.java

--- geoportal/src/com/esri/gpt/control/webharvest/protocol/factories/Agp2AgpRelaxedProtocolFactory.java
+++ geoportal/src/com/esri/gpt/control/webharvest/protocol/factories/Agp2AgpRelaxedProtocolFactory.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 Esri.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.esri.gpt.control.webharvest.protocol.factories;
 
@@ -8,8 +19,13 @@ import com.esri.gpt.catalog.harvest.protocols.HarvestProtocolAgp2Agp;
 import com.esri.gpt.control.webharvest.protocol.Protocol;
 
 /**
- *
- * @author Esri
+ * Relaxed Agp2Agp protocol factory.
+ * Relaxed protocol factory creates protocol handler which allow to continue 
+ * harvesting between two AGP servers even an exception occurred on a single
+ * item.
+ * <p/>
+ * Regular protocol factory creates handler which stops harvesting after a single
+ * error.
  */
 public class Agp2AgpRelaxedProtocolFactory extends Agp2AgpProtocolFactory {
 
