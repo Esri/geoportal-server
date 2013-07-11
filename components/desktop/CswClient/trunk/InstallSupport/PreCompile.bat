804888060f23fa9f1dad7c364acd9dbad2f85243
unknown <robe4677@PSBUILD5.esri.com>
2013-07-11 15:50:19 -0700

    Add RD command to clean up un-needed directory.

--- components/desktop/CswClient/trunk/InstallSupport/PreCompile.bat
+++ components/desktop/CswClient/trunk/InstallSupport/PreCompile.bat
@@ -4,3 +4,7 @@ RD /S /Q C:\Builds\GeoportalServer\10.2_CSWClients\Sources\Geoportal\trunk\src\g
 
 XCOPY C:\Repositories\geoportal-server\components\desktop\AppLogger\trunk\*.* C:\Builds\GeoportalServer\10.2_CSWClients\Sources\AppLogger\trunk /E /I /Y
 XCOPY C:\Repositories\geoportal-server\geoportal\src\gpt\search\profiles\*.* C:\Builds\GeoportalServer\10.2_CSWClients\Sources\Geoportal\trunk\src\gpt\search\profiles /E /I /Y
+
+RD /S /Q C:\Builds\GeoportalServer\10.2_CSWClients\Sources\CswClient\addins
+RD /S /Q C:\Builds\GeoportalServer\10.2_CSWClients\Sources\CswClient\branches
+RD /S /Q C:\Builds\GeoportalServer\10.2_CSWClients\Sources\CswClient\tags
