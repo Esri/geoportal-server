eeb608afc806d0535ee1f3b373e386bcaaca9335
Piotr Andzel <pandzel@esri.com>
2013-07-24 08:45:38 -0700

    Log message on the beginning and end of synchronization from ArcGIS Server and Portal for ArcGIS.

--- geoportal/src/com/esri/gpt/agp/ags/Ags2AgpCopy.java
+++ geoportal/src/com/esri/gpt/agp/ags/Ags2AgpCopy.java
@@ -87,6 +87,7 @@ public class Ags2AgpCopy {
    * @throws Exception if anything fails
    */
   public void copy() throws Exception {
+    LOGGER.log(Level.INFO, "Starting synchronization from ArcGIS Server "+source.getRestUrl()+" into Portal for ArcGIS "+destination.getConnection().getHost());
     RequestContext requestContext = RequestContext.extract(null);
     try {
 
@@ -113,6 +114,7 @@ public class Ags2AgpCopy {
 
     } finally {
       requestContext.onExecutionPhaseCompleted();
+      LOGGER.log(Level.INFO, "Completed synchronization from ArcGIS Server "+source.getRestUrl()+" into Portal for ArcGIS "+destination.getConnection().getHost());
     }
   }
   
