ae9d5d5aebd9abb03084f12525c0cbe202119dd8
robblis <rblischke@esri.com>
2014-09-24 15:12:17 -0700

    Update for 10.2 branch

--- components/desktop/PublishClient/branches/10.2/InstallSupport/PreCompile.bat
+++ components/desktop/PublishClient/branches/10.2/InstallSupport/PreCompile.bat
@@ -1,3 +1,3 @@
-RD /S /Q C:\Builds\GeoportalServer\10.2_PublishClient\Sources\AppLogger\trunk
+RD /S /Q C:\Builds\GeoportalServer\10.2_PublishClient\Sources\AppLogger\branches\10.2
 
-XCOPY C:\Repositories\geoportal-server\components\desktop\AppLogger\trunk\*.* C:\Builds\GeoportalServer\10.2_PublishClient\Sources\AppLogger\trunk /E /I /Y
+XCOPY C:\Repositories\geoportal-server\components\desktop\AppLogger\branches\10.2\*.* C:\Builds\GeoportalServer\10.2_PublishClient\Sources\AppLogger\branches\10.2 /E /I /Y
