1822178f5ebdcf222b0b741452e64e158f9f019f
robblis <rblischke@esri.com>
2014-09-24 15:34:56 -0700

    Update for 10.2 branch

--- components/desktop/WMCOpener/branches/10.2/InstallSupport/Package.cmd
+++ components/desktop/WMCOpener/branches/10.2/InstallSupport/Package.cmd
@@ -1,2 +1,2 @@
 echo Packaging Install
-"C:\Program Files (x86)\Installshield\2012 SAB\System\IsCmdBld.exe" -p "C:\Builds\GeoportalServer\10.2_WMCOpener\Sources\WMCOpener\trunk\InstallSupport\WMCOpener.ism" -y 0.0.0
+"C:\Program Files (x86)\Installshield\2012 SAB\System\IsCmdBld.exe" -p "C:\Builds\GeoportalServer\10.2_WMCOpener\Sources\WMCOpener\branches\10.2\InstallSupport\WMCOpener.ism" -y 0.0.0
