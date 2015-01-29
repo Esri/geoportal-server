f32b60be8b422ad69a3104d09bafa0149cb51f1c
pandzel <pandzel@esri.com>
2015-01-28 16:25:08 -0800

    ResourceLinkBuilder fixed.

--- geoportal/src/com/esri/gpt/catalog/search/ResourceLinkBuilder.java
+++ geoportal/src/com/esri/gpt/catalog/search/ResourceLinkBuilder.java
@@ -39,6 +39,8 @@ import com.esri.gpt.framework.search.DcList;
 import com.esri.gpt.framework.search.SearchXslRecord;
 import com.esri.gpt.framework.util.LogUtil;
 import com.esri.gpt.framework.util.Val;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -121,8 +123,17 @@ private String getRelativePath() {
 		if (Val.chkStr(this.baseContextPath).equals("")) {
 			return null;
 		}
-		String tmpContextPath = this.baseContextPath.replaceAll("/$", "");
-		tmpContextPath = tmpContextPath.replaceAll(".*/", "/");
+		String temp = this.baseContextPath.replaceAll("/$", "");
+		String tmpContextPath = temp.replaceAll(".*/", "/");
+        try {
+          URL url = new URL(temp);
+          String temp2 = url.getPath();
+          if (temp2==null || temp2.length()==0) {
+            temp2 = "/";
+          }
+          tmpContextPath = temp2;
+        } catch (Exception ex) {
+        }
 		return tmpContextPath;
 	} catch (Throwable e) {
 		LOG.log(Level.FINER, "", e);
