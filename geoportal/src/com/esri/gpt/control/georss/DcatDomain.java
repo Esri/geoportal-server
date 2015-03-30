ab3633271c849ed165b1423beac85a191f6ca1d3
pandzel <pandzel@esri.com>
2015-03-30 13:44:41 -0700

    DCAT1.1: DcatDomain updated to handle simple copy of the value.

--- geoportal/src/com/esri/gpt/control/georss/DcatDomain.java
+++ geoportal/src/com/esri/gpt/control/georss/DcatDomain.java
@@ -24,12 +24,17 @@ import java.util.regex.Pattern;
  * Dcat domain of the field.
  */
 public class DcatDomain extends ArrayList<DcatDomain.DcatMapping> {
+  private static final String COPY_TAG = "*";
   private static final Logger L = Logger.getLogger(DcatDomain.class.getCanonicalName());
   
   public void addMapping(String from, String value) {
     try {
       Pattern fromPattern = Pattern.compile(from,Pattern.CASE_INSENSITIVE);
-      add(new DcatMapping(fromPattern, value));
+      if (COPY_TAG.equals(value)) {
+        add(new DcatMapping(fromPattern));
+      } else {
+        add(new DcatMapping(fromPattern, value));
+      }
     } catch (Exception ex) {
       L.log(Level.WARNING, "Invalid DCAT domain pattern: "+from, ex);
     }
@@ -38,7 +43,7 @@ public class DcatDomain extends ArrayList<DcatDomain.DcatMapping> {
   public String translate(String value) {
     for (DcatDomain.DcatMapping mapping: this) {
       if (mapping.matches(value)) {
-        return mapping.getValue();
+        return mapping.getValue(value);
       }
     }
     return value;
@@ -46,19 +51,55 @@ public class DcatDomain extends ArrayList<DcatDomain.DcatMapping> {
   
   public static class DcatMapping {
     private final Pattern pattern;
-    private final String value;
+    private final IDcatGetter valueGetter;
+
+    public DcatMapping(Pattern pattern, IDcatGetter valueGetter) {
+      this.pattern = pattern;
+      this.valueGetter = valueGetter!=null? valueGetter: new DcatCopyGetter();
+    }
 
     public DcatMapping(Pattern pattern, String value) {
       this.pattern = pattern;
-      this.value = value;
+      this.valueGetter = new DcatFixedGetter(value);
     }
 
-    public String getValue() {
-      return value;
+    public DcatMapping(Pattern pattern) {
+      this.pattern = pattern;
+      this.valueGetter = new DcatCopyGetter();
+    }
+
+    public String getValue(String input) {
+      return valueGetter.get(input);
     }
     
     public boolean matches(String input) {
       return pattern.matcher(input).matches();
     }
+  }
+  
+  public static interface IDcatGetter {
+    String get(String input);
+  }
+  
+  public static class DcatFixedGetter implements IDcatGetter {
+    private final String value;
+
+    public DcatFixedGetter(String value) {
+      this.value = value;
+    }
+
+    @Override
+    public String get(String input) {
+      return value;
+    }
+  }
+  
+  public static class DcatCopyGetter implements IDcatGetter {
+
+    @Override
+    public String get(String input) {
+      return input;
+    }
+    
   }
 }
