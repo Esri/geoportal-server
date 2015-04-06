94854700a2690d9f831832f59559d4ee47e5be8c
robblis <rblischke@esri.com>
2015-04-06 15:52:28 -0700

    Update for 10.3.1 build

--- components/desktop/WMCOpener/trunk/InstallSupport/PreCompile.bat
+++ components/desktop/WMCOpener/trunk/InstallSupport/PreCompile.bat
@@ -1,3 +1,3 @@
-RD /S /Q C:\Builds\GeoportalServer\10.3_WMCOpener\Sources\AppLogger\trunk
+RD /S /Q C:\Builds\GeoportalServer\10.3.1_WMCOpener\Sources\AppLogger\trunk
 
-XCOPY C:\Repositories\geoportal-server\components\desktop\AppLogger\trunk\*.* C:\Builds\GeoportalServer\10.3_WMCOpener\Sources\AppLogger\trunk /E /I /Y
+XCOPY C:\Repositories\geoportal-server\components\desktop\AppLogger\trunk\*.* C:\Builds\GeoportalServer\10.3.1_WMCOpener\Sources\AppLogger\trunk /E /I /Y
