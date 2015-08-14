352f11bf115af253b695ead56f17bfeb5d9ada12
Urban MacGillivray <umacgillivray@esri.com>
2015-08-14 11:36:26 -0700

    Geoportal to Portal synchronization.

--- geoportal/src/com/esri/gpt/agp/sync/Gpt2AgpPush.java
+++ geoportal/src/com/esri/gpt/agp/sync/Gpt2AgpPush.java
@@ -20,9 +20,11 @@ import com.esri.gpt.agp.client.AgpProperty;
 import com.esri.gpt.agp.client.AgpUtil;
 import com.esri.gpt.agp.multipart2.MultipartProvider;
 import com.esri.gpt.framework.context.RequestContext;
+import com.esri.gpt.framework.http.HttpClientRequest;
 import com.esri.gpt.framework.sql.IClobMutator;
 import com.esri.gpt.framework.sql.ManagedConnection;
 import com.esri.gpt.framework.util.Val;
+import com.esri.gpt.framework.xml.DomUtil;
 import com.esri.gpt.framework.xml.XsltTemplate;
 import com.esri.gpt.framework.xml.XsltTemplates;
 
@@ -40,6 +42,9 @@ import javax.xml.transform.TransformerException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  * A push synchronizer from a source Geoportal to a destination ArcGIS Portal.
@@ -408,11 +413,365 @@ public class Gpt2AgpPush {
     if (jsoItem.has("_thumbnailurl") && (!jsoItem.isNull("_thumbnailurl"))) {
     	String s = Val.chkStr(jsoItem.getString("_thumbnailurl"));
     	if (s.startsWith("http://") || (s.startsWith("https://"))) {
-    		System.err.println("*** "+s);
+    		//System.err.println("*** "+s);
     		sourceItem.getProperties().add(new AgpProperty("thumbnailurl",s));
     	}
     }
 
+  }
+  
+  private void parseWmsLayer(Node layerNode) throws Exception {
+  	String s, name = null, title = null, abs = null;
+    NodeList nl = layerNode.getChildNodes(); 
+    for (int i=0; i<nl.getLength(); i++) {
+      Node nd = nl.item(i);
+      if (nd.getNodeType() != Node.ELEMENT_NODE) continue;
+      String ln = Val.chkStr(nd.getNodeName());
+      if (ln.equalsIgnoreCase("Name")) {
+  			s = Val.chkStr(nd.getTextContent());
+  			if (s.length() > 0) name = s;
+      } else if (ln.equalsIgnoreCase("Title")) {
+  			s = Val.chkStr(nd.getTextContent());
+  			if (s.length() > 0) title = s;
+      } else if (ln.equalsIgnoreCase("Abstract")) {
+  			s = Val.chkStr(nd.getTextContent());
+  			if (s.length() > 0) abs = s;
+      } else if (ln.equalsIgnoreCase("BoundingBox")) {
+      } else if (ln.equalsIgnoreCase("SRS") || ln.equalsIgnoreCase("CRS")) {
+        // supported spatial references
+        // <SRS>EPSG:4326</SRS> or <SRS>EPSG:4326 EPSG:32624 EPSG:32661</SRS>
+      	
+      } else if (ln.equalsIgnoreCase("Style")) {
+      } else if (ln.equalsIgnoreCase("Layer")) {
+      	parseWmsLayer(nd);
+      }
+    }
+  }
+  
+  private void readCapabilities(AgpItem destItem) throws Exception {
+  	
+  	// https://devtopia.esri.com/WebGIS/arcgis-js-api/blob/master/esri/layers/WMSLayer.js
+  	// https://devtopia.esri.com/WebGIS/arcgis-portal-app/blob/7bf69cd63a85c89f9ccc5456d7b17b691340fcbd/src/js/arcgisonline/sharing/dijit/dialog/AddItemDlg.js
+    // _addOGCUrl, _getWMSData, _loadOGC, _getOGCServiceInfo
+  	
+  	/*
+
+      item = dojo.mixin(item,{
+        "url"              : wmsLayer.url,
+        "description"      : wmsLayer.description || "",
+        "accessInformation": wmsLayer.copyright || "",
+        "text"             : this._getWMSData(wmsLayer, selectedLayers),
+        "extent"           : fullExtent
+      });
+      
+      
+    _getWMSData: function(wmsLayer, selectedLayers){
+//    return dojo.toJson({
+      return dojo.json.stringify({
+        "title" : wmsLayer.title || "",
+        "url" : wmsLayer.url,
+        "mapUrl" : wmsLayer.getMapURL,
+        "version" : wmsLayer.version,
+        "layers" : selectedLayers,
+        "copyright" : wmsLayer.copyright || "",
+        "maxHeight" : wmsLayer.maxHeight,
+        "maxWidth" : wmsLayer.maxWidth,
+        "spatialReferences" : wmsLayer.spatialReferences,
+        "format" : wmsLayer.getImageFormat() !== "png" ? wmsLayer.getImageFormat() : null
+      });
+    },
+
+  	 */
+  	
+  	String url = Val.chkStr(destItem.getProperties().getValue("url"));
+  	
+  	url = "http://inspiresrv4:6080/arcgis/services/abu/MapServer/WMSServer?request=GetCapabilities&service=WMS";
+  	
+  	//url = "http://nowcoast.noaa.gov/wms/com.esri.wms.Esrimap/obs?request=GetCapabilities&service=WMS";
+  	
+  	HttpClientRequest http = new HttpClientRequest();
+  	http.setUrl(url);
+  	String xml = http.readResponseAsCharacters();
+  	System.err.println("xml:\r\n"+xml);
+  	
+  	JSONObject data = new JSONObject();
+  	data.put("version","1.3.0");
+  	data.put("title","");
+  	data.put("url","");
+  	data.put("mapUrl","");
+  	data.put("copyright","");
+  	data.put("maxWidth",5000);
+  	data.put("maxHeight",5000);
+  	data.put("format",JSONObject.NULL);
+  	data.put("layers",JSONObject.NULL);
+  	data.put("spatialReferences",JSONObject.NULL);
+  	
+  	// item: extent, accessInformation, description, url
+  	
+  	Document dom = DomUtil.makeDomFromString(xml,true);
+  	Node root = dom.getDocumentElement();
+  	
+  	if (root != null) {
+  		String ln = Val.chkStr(root.getNodeName());
+  		if (ln.equals("WMT_MS_Capabilities")) {
+  		} else if (ln.equals("WMS_Capabilities")) {
+  		} else {
+  			// TODO logging here
+  			root = null;
+  		}
+  	}
+  	if (root == null) return;
+  	
+  	String version = null, title = null, name = null, sAbstract = null, getmapUrl = null;
+  	Node ndv = root.getAttributes().getNamedItem("version");
+  	if (ndv != null) version = ndv.getNodeValue();
+  	if (version == null || version.length() == 0) version = "1.3.0";
+  	//System.err.println("****version="+version);
+  	
+  	String s; int n;
+    NodeList nl = root.getChildNodes(); 
+    for (int i=0; i<nl.getLength(); i++) {
+      Node nd = nl.item(i);
+      if (nd.getNodeType() == Node.ELEMENT_NODE) { 
+        //String ns = Val.chkStr(nd.getNamespaceURI());
+        String ln = Val.chkStr(nd.getNodeName());
+        if (ln.equalsIgnoreCase("Service")) {
+          NodeList nl2 = nd.getChildNodes(); 
+          for (int i2=0; i2<nl2.getLength(); i2++) {
+          	Node nd2 = nl2.item(i2);
+          	if (nd2.getNodeType() == Node.ELEMENT_NODE) { 
+          		String ln2 = Val.chkStr(nd2.getNodeName());
+          		if (ln2.equalsIgnoreCase("Name")) {
+          			s = Val.chkStr(nd2.getTextContent());
+          			if (s.length() > 0) name = s;
+          		} else if (ln2.equalsIgnoreCase("Title")) {
+          			s = Val.chkStr(nd2.getTextContent());
+          			if (s.length() > 0) {
+          				title = s;
+          				data.put("title",s);
+          			}
+          		} else if (ln2.equalsIgnoreCase("Abstract")) {
+          			s = Val.chkStr(nd2.getTextContent());
+          			if (s.length() > 0) sAbstract = s;
+          		} else if (ln2.equalsIgnoreCase("AccessConstraints")) {
+          			s = Val.chkStr(nd2.getTextContent());
+          			if (s.length() > 0) data.put("copyright",s);
+          		} else if (ln2.equalsIgnoreCase("MaxWidth")) {
+          			s = Val.chkStr(nd2.getTextContent());
+          			if (s.length() > 0) {
+          				n = Val.chkInt(s,-1);
+          				if (n > 0) data.put("maxWidth",n);
+          			}
+          		} else if (ln2.equalsIgnoreCase("MaxHeight")) {
+          			s = Val.chkStr(nd2.getTextContent());
+          			if (s.length() > 0) {
+          				n = Val.chkInt(s,-1);
+          				if (n > 0) data.put("maxHeight",n);
+          			}
+          		}
+          	}
+          }
+        } else if (ln.equalsIgnoreCase("Capability")) {
+          NodeList nl2 = nd.getChildNodes(); 
+          for (int i2=0; i2<nl2.getLength(); i2++) {
+          	Node nd2 = nl2.item(i2);
+          	if (nd2.getNodeType() == Node.ELEMENT_NODE) { 
+          		String ln2 = Val.chkStr(nd2.getNodeName());
+          		if (ln2.equalsIgnoreCase("Request")) {
+          			Node ndGetMap = DomUtil.findFirst(nd2,"GetMap");
+          			if (ndGetMap != null) {
+          				Node[] dcps = DomUtil.findChildren(ndGetMap,"DCPType");
+          				for (Node dcp: dcps) {
+          					Node[] https = DomUtil.findChildren(dcp,"HTTP");
+            				for (Node ndHttp: https) {
+            					Node ndGet = DomUtil.findFirst(ndHttp,"Get");
+            					if (ndGet != null) {
+            						Node ndRes = DomUtil.findFirst(ndGet,"OnlineResource");
+            						if (ndRes != null) {
+            							Node ndx = ndRes.getAttributes().getNamedItemNS("http://www.w3.org/1999/xlink","href");
+            							if (ndx != null) {
+            								s = Val.chkStr(ndx.getNodeValue());
+            								if ((getmapUrl == null) && (s.length() > 0)) {
+            									getmapUrl = s;
+            									// TODO: 
+            									// this.getMapURL = this._stripParameters(getMapHREF, ["service", "request"]);
+            									System.err.println("**************getmapUrl="+getmapUrl);
+            								}
+            							}
+            						}
+            					}
+            				}
+          				}
+          				
+          				/*
+          				Node[] fmts = DomUtil.findChildren(ndGetMap,"Format");
+          				for (Node fmt: fmts) {
+    								s = Val.chkStr(fmt.getTextContent());
+    								if (s.length() > 0) {
+    									// TODO check the formats
+    									//System.err.println("**************format="+s);
+								      // make sure the format we want is supported; otherwise switch
+								      //if (!array.some(this.getMapFormats, function(el){
+								        // also support: <Format>image/png; mode=24bit</Format>
+								      //  return el.indexOf(this.imageFormat) > -1;
+								      //}, this)) {
+								      //  this.imageFormat = this.getMapFormats[0];
+								      //}
+    								}
+          				}
+          				*/
+          				
+          			}
+          		} else if (ln2.equalsIgnoreCase("Layer")) {
+          			// "LatLonBoundingBox"
+/*
+
+					      var result = new WMSLayerInfo();
+					      result.name = "";
+					      result.title = "";
+					      result.description = "";
+					      result.allExtents = [];
+					      result.spatialReferences = [];
+					      result.subLayers = []; // not sure why this has to be done
+					      // all services have LatLonBoundingBox or EX_GeographicBoundingBox (might not be on the first layer ...)
+					      var latLonBoundingBox = this._getTag("LatLonBoundingBox", layerXML);
+					      if (latLonBoundingBox) {
+					        result.allExtents[0] = this._getExtent(latLonBoundingBox, 4326);
+					      }
+					      var geographicBoundingBox = this._getTag("EX_GeographicBoundingBox", layerXML);
+					      var extent;
+					      if (geographicBoundingBox) {
+					        extent = new Extent(0, 0, 0, 0, new SpatialReference({
+					          wkid: 4326
+					        }));
+					        extent.xmin = parseFloat(this._getTagValue("westBoundLongitude", geographicBoundingBox, 0));
+					        extent.ymin = parseFloat(this._getTagValue("southBoundLatitude", geographicBoundingBox, 0));
+					        extent.xmax = parseFloat(this._getTagValue("eastBoundLongitude", geographicBoundingBox, 0));
+					        extent.ymax = parseFloat(this._getTagValue("northBoundLatitude", geographicBoundingBox, 0));
+					        result.allExtents[0] = extent;
+					      }
+					      if (!latLonBoundingBox && !geographicBoundingBox) {
+					        // not according to spec
+					        extent = new Extent(-180, -90, 180, 90, new SpatialReference({
+					          wkid: 4326
+					        }));
+					        result.allExtents[0] = extent;
+					      }
+					      result.extent = result.allExtents[0];
+					      
+
+
+					      //var srAttrName = (this.version == "1.3.0") ? "CRS" : "SRS";
+					      var srAttrName = (array.indexOf(["1.0.0","1.1.0","1.1.1"],this.version) > -1) ? "SRS" : "CRS";
+					      array.forEach(layerXML.childNodes, function(childNode){
+					        if (childNode.nodeName == "Name") {
+					          // unique name
+					          result.name = (childNode.text ? childNode.text : childNode.textContent) || "";
+					        } else if (childNode.nodeName == "Title") {
+					          // title
+					          result.title = (childNode.text ? childNode.text : childNode.textContent) || "";
+					        } else if (childNode.nodeName == "Abstract") {
+					          //description
+					          result.description = (childNode.text ? childNode.text : childNode.textContent) || "";
+					          
+					        } else if (childNode.nodeName == "BoundingBox") {
+					          // other extents
+					          // <BoundingBox CRS="CRS:84" minx="-164.765831" miny="25.845557" maxx="-67.790980" maxy="70.409756"/>  
+					          // <BoundingBox CRS="EPSG:4326" minx="25.845557" miny="-164.765831" maxx="70.409756" maxy="-67.790980"/>  
+					          var srAttr = childNode.getAttribute(srAttrName), wkid;
+					          if (srAttr && srAttr.indexOf("EPSG:") === 0) {
+					            wkid = parseInt(srAttr.substring(5), 10);
+					            if (wkid !== 0 && !isNaN(wkid)) {
+					              var extent;
+					              if (this.version == "1.3.0") {
+					                extent = this._getExtent(childNode, wkid, this._useLatLong(wkid));
+					              } else {
+					                extent = this._getExtent(childNode, wkid);
+					              }
+					              result.allExtents[wkid] = extent;
+					              if (!result.extent) {
+					                result.extent = extent; // only first one
+					              }
+					            }
+					          } else if (srAttr && srAttr.indexOf("CRS:") === 0) {
+					            wkid = parseInt(srAttr.substring(4), 10);
+					            if (wkid !== 0 && !isNaN(wkid)) {
+					              if (this._CRS_TO_EPSG[wkid]) {
+					                wkid = this._CRS_TO_EPSG[wkid];
+					              }
+					              result.allExtents[wkid] = this._getExtent(childNode, wkid);
+					            }
+					          } else {
+					            wkid = parseInt(srAttr, 10);
+					            if (wkid !== 0 && !isNaN(wkid)) {
+					              result.allExtents[wkid] = this._getExtent(childNode, wkid);
+					            }
+					          }
+					          
+					        } else if (childNode.nodeName == srAttrName) {
+					          // supported spatial references
+					          // <SRS>EPSG:4326</SRS> or <SRS>EPSG:4326 EPSG:32624 EPSG:32661</SRS>
+					          var value = childNode.text ? childNode.text : childNode.textContent; // EPSG:102100
+					          var arr = value.split(" ");
+					          array.forEach(arr, function(val){
+					            if (val.indexOf(":") > -1) {
+					              val = parseInt(val.split(":")[1], 10);
+					            } else {
+					              val = parseInt(val, 10);
+					            }
+					            if (val !== 0 && !isNaN(val)) { // val !== 84 && 
+					              if (this._CRS_TO_EPSG[val]) {
+					                val = this._CRS_TO_EPSG[val];
+					              }
+					              if (array.indexOf(result.spatialReferences, val) == -1) {
+					                result.spatialReferences.push(val);
+					              }
+					            }
+					          }, this);
+					          
+					        } else if (childNode.nodeName == "Style") {
+					          // legend URL
+					          var legendXML = this._getTag("LegendURL", childNode);
+					          if (legendXML) {
+					            var onlineResourceXML = this._getTag("OnlineResource", legendXML);
+					            if (onlineResourceXML) {
+					              result.legendURL = onlineResourceXML.getAttribute("xlink:href");
+					            }
+					          }
+					          
+					        } else if (childNode.nodeName === "Layer") {
+					          // sub layers
+					          result.subLayers.push(this._getLayerInfo(childNode));
+					        }
+					      }, this);
+					
+					      result.title = result.title || result.name; 
+					          
+					      return result;
+      
+
+ */
+          			
+          			
+          			
+          			
+          			
+          			
+          		}
+          	}
+          }
+        }
+      }
+    }
+    if (title == null) {
+    	if (name != null) {
+    		data.put("title",name);
+    	}
+    }
+        
+  	String text = data.toString(1);
+  	System.err.println("****text\r\n"+text);
+    
   }
   
   /**
@@ -515,8 +874,12 @@ public class Gpt2AgpPush {
     //System.err.println(destItem.getProperties());
     boolean bRequiresUpdate = this.itemHelper.requiresUpdate(sourceItem,dest,destItem);
     if (this.forceUpdates || bRequiresUpdate) {
+    	
+    	 //this.readCapabilities(destItem);
+    	 
     	 //System.err.println("update.......................");
        this.execPublishItem(sourceItem,destItem);
+    	 
        // TODO publish metadata
        //this.execPublishMetadata(sDestId,sXml);
       return true;
@@ -545,6 +908,7 @@ public class Gpt2AgpPush {
       this.resourceDataTable =  this.context.getCatalogConfiguration().getResourceDataTableName();
       	
     	String sql = "SELECT DOCUUID,UPDATEDATE,APPROVALSTATUS,PROTOCOL_TYPE,FINDABLE FROM "+this.resourceTable;
+    	sql += " WHERE DOCUUID ='{81727D9E-AFBD-4829-B185-5C74E7DCFFA2}'";
       st = this.con.prepareStatement(sql);
       ResultSet rs = st.executeQuery();     
       while (rs.next()) {
@@ -567,7 +931,7 @@ public class Gpt2AgpPush {
         	//System.err.println(xml);
         	if ((xml != null) && (xml.length() > 0)) {
         		String result = Val.chkStr(xslt.transform(xml));
-        		System.err.println(result);
+        		//System.err.println(result);
         		if ((result != null) && (result.length() > 0)) {
         			try {
         				JSONObject jsoItem = new JSONObject(result); 
@@ -575,7 +939,7 @@ public class Gpt2AgpPush {
                 sourceItem.getProperties().add(new AgpProperty("modified",""+mod.getTime()));
                 this.prepareItem(jsoItem,sourceItem);
                 //System.err.println("type:"+sourceItem.getProperties().getValue("type"));
-                System.err.println(sourceItem.getProperties());
+                //System.err.println(sourceItem.getProperties());
                 this.syncItem(sourceItem,uuid,xml);
         			} catch (Exception ejson) {
         				this.numWithXsltError++;
