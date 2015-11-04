4f50f33502c1b8f33cfdea593dd82e50255bd232
pandzel <pandzel@esri.com>
2015-11-04 14:39:58 -0800

    CKAN: Test unit.

--- geoportal/test/com/esri/gpt/framework/ckan/CkanTest.java
+++ geoportal/test/com/esri/gpt/framework/ckan/CkanTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 pete5162.
+ * Copyright 2013 Esri.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,40 +18,84 @@ package com.esri.gpt.framework.ckan;
 import com.esri.gpt.framework.http.StringHandler;
 import com.esri.gpt.framework.http.crawl.HttpCrawlRequest;
 import com.esri.gpt.framework.robots.Bots;
-import com.esri.gpt.framework.robots.BotsMode;
-import com.esri.gpt.framework.robots.BotsUtils;
 import java.io.IOException;
 import java.net.URL;
 import org.apache.commons.httpclient.HttpMethodBase;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
- * @author pete5162
+ * CKAN Test.
  */
 public class CkanTest {
 
+  protected void onPackage(CkanPackage pkg) throws Exception {
+    assertNotNull(pkg);
+    System.out.println("" + pkg);
+    for (CkanResource res : pkg.getResources()) {
+      onResource(res);
+    }
+  }
+
+  protected void onResource(CkanResource res) {
+    System.out.println("  " + res);
+  }
+
   @Test
   public void ckanTest() throws Exception {
-    String baseUrl = "http://demo.ckan.org/api/3/action";
+    //String baseUrl = "http://demo.ckan.org/api/3/action";
     //String baseUrl = "http://catalog.data.gov/api/3/action";
-    
-    CkanPackageList packageList = readPackageList(new URL(baseUrl+"/package_list"));
-    assertNotNull(packageList);
-    
-    for (String packageId: packageList.getPackagesIds()) {
-      CkanPackage pkg = readPackage(new URL(baseUrl+"/package_show?id="+packageId));
-      assertNotNull(pkg);
-      System.out.println(pkg);
-      for (CkanResource rsc : pkg.getResources()) {
-        System.out.println("   "+rsc);
+    runTest("http://demo.ckan.org/api/3/action");
+  }
+
+  public void runTest(String baseUrl) throws Exception {
+    JSONObject packageList = readJsonData(new URL(baseUrl + "/package_list"));
+    if (packageList.has("result")) {
+      if (packageList.get("result") instanceof JSONArray) {
+        // result of /package_list
+        JSONArray idsArray = packageList.getJSONArray("result");
+        for (int i = 0; i < idsArray.length(); i++) {
+          String packageId = idsArray.getString(i);
+          JSONObject pkgObject = readJsonData(new URL(baseUrl + "/package_show?id=" + packageId));
+          if (pkgObject.has("result") && pkgObject.get("result") instanceof JSONObject) {
+            CkanPackage pkg = new CkanPackageAdaptor(pkgObject.getJSONObject("result"));
+            onPackage(pkg);
+          }
+        }
+      } else if (packageList.get("result") instanceof JSONObject) {
+        // result of /package_search
+        JSONObject result = packageList.getJSONObject("result");
+        if (result.has("results") && result.get("results") instanceof JSONArray) {
+          long count = result.getLong("count");
+          long start = 0;
+          do {
+            JSONArray pkgs = result.getJSONArray("results");
+            for (int i = 0; i < pkgs.length(); i++) {
+              JSONObject pkgObject = pkgs.getJSONObject(i);
+              CkanPackage pkg = new CkanPackageAdaptor(pkgObject);
+              onPackage(pkg);
+            }
+            start = start + pkgs.length();
+            if (start < count) {
+              JSONObject searchData = readJsonData(new URL(baseUrl + "/package_search?start=" + start));
+              if (!(searchData.has("result") && searchData.get("result") instanceof JSONObject)) {
+                break;
+              }
+              result = searchData.getJSONObject("result");
+              if (!(result.has("results") && result.get("results") instanceof JSONArray)) {
+                break;
+              }
+            }
+          } while (start < count);
+        }
       }
     }
- }
-  
-  private CkanPackageList readPackageList(URL packageListUrl) throws IOException, JSONException {
+  }
+
+  private JSONObject readJsonData(URL url) throws IOException, JSONException {
     Bots bots = null; //BotsUtils.readBots(BotsMode.always, packageListUrl);
     HttpCrawlRequest request = new HttpCrawlRequest(bots) {
       @Override
@@ -61,27 +105,10 @@ public class CkanTest {
         return method;
       }
     };
-    request.setUrl(packageListUrl.toExternalForm());
+    request.setUrl(url.toExternalForm());
     StringHandler handler = new StringHandler();
     request.setContentHandler(handler);
     request.execute();
-    return CkanParser.parsePackageList(handler.getContent());
-  }
-  
-  private CkanPackage readPackage(URL packageUrl)  throws IOException, JSONException {
-    Bots bots = null; //BotsUtils.readBots(BotsMode.always, packageUrl);
-    HttpCrawlRequest request = new HttpCrawlRequest(bots) {
-      @Override
-      protected HttpMethodBase createMethod() throws IOException {
-        HttpMethodBase method = super.createMethod();
-        method.addRequestHeader("User-Agent", "GeoportalServer");
-        return method;
-      }
-    };
-    request.setUrl(packageUrl.toExternalForm());
-    StringHandler handler = new StringHandler();
-    request.setContentHandler(handler);
-    request.execute();
-    return CkanParser.parsePackage(handler.getContent());
+    return new JSONObject(handler.getContent());
   }
 }
