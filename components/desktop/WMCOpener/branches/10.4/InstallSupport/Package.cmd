cc0488a23b7e61a1561b1358d92952ab99ed25cb
zguo <zguo@esri.com>
2016-06-02 10:11:39 -0700

    create branch for 10.4

---
+++ components/desktop/WMCOpener/branches/10.4/InstallSupport/Package.cmd
@@ -0,0 +1,2 @@
+echo Packaging Install
+"C:\Program Files (x86)\InstallShield\2012SpringSP1 SABSystem\IsCmdBld.exe" -p "C:\Builds\GeoportalServer\10.4_WMCOpener\Sources\WMCOpener\trunk\InstallSupport\WMCOpener.ism" -y 0.0.0
