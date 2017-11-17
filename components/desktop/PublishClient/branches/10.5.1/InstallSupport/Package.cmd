7405186bd8cea59631e02c259afd3a1efa474e2b
zguo <zguo@esri.com>
2017-11-17 13:37:27 -0800

    create branch and use trunk for 10.6

---
+++ components/desktop/PublishClient/branches/10.5.1/InstallSupport/Package.cmd
@@ -0,0 +1,2 @@
+echo Packaging Install
+"C:\Program Files (x86)\InstallShield\2014 SAB\System\IsCmdBld.exe" -p "C:\Builds\GeoportalServer\10.5_PublishClient\Sources\PublishClient\trunk\InstallSupport\PublishClient.ism" -y 1.2.7.300
