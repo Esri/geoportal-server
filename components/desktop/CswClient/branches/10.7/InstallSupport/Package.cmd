1f4cc5a91a63a746d7a043497935f484658a6012
zguo <zguo@esri.com>
2019-05-20 10:56:33 -0700

    create archive for 10.7 and use trunk for 10.7.1

---
+++ components/desktop/CswClient/branches/10.7/InstallSupport/Package.cmd
@@ -0,0 +1,2 @@
+echo Packaging Install
+"C:\Program Files (x86)\InstallShield\2014 SAB\System\IsCmdBld.exe" -p "C:\Builds\GeoportalServer\10.5_CSWClients\Sources\CswClient\trunk\InstallSupport\CSWClients.ism" -y 0.0.0
---
+++ components/desktop/WMCOpener/branches/10.7/InstallSupport/PreCompile.bat
@@ -0,0 +1,3 @@
+RD /S /Q C:\Builds\GeoportalServer\10.5_WMCOpener\Sources\AppLogger\trunk
+
+XCOPY C:\Repositories\geoportal-server\components\desktop\AppLogger\trunk\*.* C:\Builds\GeoportalServer\10.5_WMCOpener\Sources\AppLogger\trunk /E /I /Y
